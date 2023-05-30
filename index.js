ecd0e3df7c197e3a19efd3b4b708999ba9e632b8
simanto789 <hazrat.ali789@gmail.com>
2023-05-30 16:56:11 +0600

    post cart item and delete the unnecessery cart

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arc8i5u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,7 @@ async function run() {
 
         const menuCollection = client.db("centralDb").collection("menu");
         const reviewCollection = client.db("centralDb").collection("reviews");
+        const cartCollection = client.db("centralDb").collection("carts");
 
         app.get('/menu', async( req,res) => {
             const result = await menuCollection.find().toArray();
@@ -36,6 +37,31 @@ async function run() {
         
         app.get('/reviews', async( req,res) => {
             const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+        // cart collection apis
+        app.get('/carts',async( req,res) => {
+            const email = req.query.email;
+            if(!email){
+                res.send([]);
+            }
+            const query = {email: email} ;
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/carts', async(req,res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
